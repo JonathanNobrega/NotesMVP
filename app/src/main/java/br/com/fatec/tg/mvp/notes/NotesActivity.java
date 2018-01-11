58d952cea6dd41db030faca35f456bf24a02ec7b
Jonathan Nobrega <jonathan.natj@gmail.com>
2018-01-11 11:26:25 -0200

    Criação do layout da tela inicial

---
+++ app/src/main/java/br/com/fatec/tg/mvp/notes/NotesActivity.java
@@ -0,0 +1,13 @@
+package br.com.fatec.tg.mvp.notes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
